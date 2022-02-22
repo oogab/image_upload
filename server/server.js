836ddc46f9e258a1ecea8a7b76b032d9712df8e0
oogab <38171845+oogab@users.noreply.github.com>
2022-02-22 14:37:19 +0900

    이미지 저장하기 전 필터 걸어주기

--- server/server.js
+++ server/server.js
@@ -7,7 +7,23 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, './uploads'),
   filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
 })
-const upload = multer({ storage })
+// const upload = multer({
+//   storage,
+//   fileFilter: (req, file, cb) => {
+//     if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") cb(null, true)
+//     else  cb(new Error("invalid file type."), false)
+//   }
+// })
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (["image/png", "image/jpeg"].includes(file.mimetype)) cb(null, true)
+    else  cb(new Error("invalid file type."), false)
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  }
+})
 
 const app = express()
 const PORT = 5001
