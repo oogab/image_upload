2f21f5252e94cefc000a5bcdbd8dd670b556ccd3
oogab <38171845+oogab@users.noreply.github.com>
2022-02-22 14:20:12 +0900

    uuid로 이미지 고유 아이디 생성해주기

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const multer = require('multer')
-const upload = multer({ dest: "uploads" })
+const { v4: uuid } = require('uuid')
+const mime = require('mime-types')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, './uploads'),
+  filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+})
+const upload = multer({ storage })
 
 const app = express()
 const PORT = 5001
